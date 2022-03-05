5b2ec834e02d3c4e793729bdefa89cfbd821aeba
emt0rres <emilee@hatchweb.com>
2022-03-04 19:26:06 -0700

    updates

---
+++ chamber/js/directory.js
@@ -0,0 +1,46 @@
+const requestURL = 'https://github.com/emt0rres/wdd230/blob/main/chamber/data/directory.json';
+
+const cards = document.querySelector('.businessCards');
+
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    console.table(jsonObject);
+    const companies = jsonObject['companies'];
+    companies.forEach(displayCompanies);
+  });
+
+function displayCompanies(company) {
+    
+  // Create elements to add to the document
+    let card = document.createElement('section');
+    let companyLogo = document.createElement('img');
+    let companyName = document.createElement('h2');
+
+
+    
+
+    // Change the textContent property of the h2 element to contain the prophet's full name
+    companyName.innerHTML = `${company.name}`;
+    
+    // Build the image attributes by using the setAttribute method for the src, alt, and loading attribute values. (Fill in the blank with the appropriate variable).
+      // element.setAttribute(name, value)
+
+    companyLogo.setAttribute('src', company.imageurl);
+    companyLogo.setAttribute('alt', 'Logo of ' + `${company.name}`);
+    companyLogo.setAttribute('loading', 'lazy');
+  
+
+
+    // Add/append the section(card) with the h2 element
+      // element.appendChild(node)
+    
+    card.appendChild(companyLogo);
+    card.appendChild(companyName);
+    
+    // Add/append the existing HTML div with the cards class with the section(card)
+      // node.appendChild(node)
+    cards.appendChild(card);
+}
