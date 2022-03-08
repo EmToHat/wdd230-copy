1ad67a548898ab0ea59513e0d5728d159bb38ff3
emt0rres <emilee@hatchweb.com>
2022-03-08 14:52:14 -0700

    updates

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -8,12 +8,18 @@ s = wind spped in miles per hour
 */ 
 
 // Variables
-let temp = document.getElementById("temp");
-let speed = document.getElementById("speed");
-let chill = document.getElementById("chill");
+let t = document.getElementById("temp");
+let s = document.getElementById("speed");
+
+let speed = parseInt(s);
+let temp = parseInt(t);
 
 // Computes the wind chill
-let f = 35.74 + 0.6215 * t - 35.75 * s * Math.pow(0.16) + 0.4275 * t * s * Math.pow(0.16);
-
-
-document.getElementById("chill").innerHTML = f;
+if (temp <= 50 && speed > 3){
+    let wc = 35.74 + 0.6215 * temp - 35.75 * Math.pow(speed,0.16) + 0.4275 * temp * Math.pow(s,0.16)
+    
+    document.getElementById("chill").innerHTML = `${Math.round(wc)}&#176;`;
+}
+else {
+    document.getElementById("chill").innerHTML = "N/A"
+}
