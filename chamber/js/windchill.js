8d04b34583e70ff2074ba32c7a8f47222ddfe3ba
emt0rres <emilee@hatchweb.com>
2022-03-12 21:21:23 -0700

    updates to api and index

--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -23,3 +23,64 @@ if (temp <= 50 && speed > 3){
 else {
     document.getElementById("chill").innerHTML = "N/A"
 }
+
+
+
+// OpenWeatherMap API Key
+const apiKey = '5e8bfdfbf705960fd8680dfebfbf0726'; 
+// OpenWeatherMap API URL
+const apiURL = 'https://api.openweathermap.org/data/2.5/weather';
+
+const stGeorge = "5546220";
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        console.log(jsObject);
+    });
+
+
+    function getWeather() {
+        let temperature = document.getElementById('temp');
+        let description = document.getElementById("description");
+        let location = document.getElementById("location");
+      
+        let api = "https://api.openweathermap.org/data/2.5/weather";
+        let apiKey = '5e8bfdfbf705960fd8680dfebfbf0726';
+      
+        location.innerHTML = "Locating...";
+      
+        navigator.geolocation.getCurrentPosition(success, error);
+      
+        function success(position) {
+          latitude = position.coords.latitude;
+          longitude = position.coords.longitude;
+      
+          let url =
+            api +
+            "?lat=" +
+            latitude +
+            "&lon=" +
+            longitude +
+            "&appid=" +
+            apiKey +
+            "&units=imperial";
+      
+          fetch(url)
+            .then(response => response.json())
+            .then(data => {
+              console.log(data);
+              let temp = data.main.temp;
+              temperature.innerHTML = temp + "° F";
+              location.innerHTML =
+                data.name + " (" + latitude + "°, " + longitude + "°)";
+              description.innerHTML = data.weather[0].main;
+            });
+        }
+      
+        function error() {
+          location.innerHTML = "Unable to retrieve your location";
+        }
+      }
+      
+      getWeather();
