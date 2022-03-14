8324df866a162b5b48f4144f37e1bcb31c78acfc
emt0rres <emilee@hatchweb.com>
2022-03-14 16:36:22 -0600

    updates

--- chamber/js/weatherapi.js
+++ chamber/js/weatherapi.js
@@ -1,24 +1,24 @@
-let apiURL = 'https://api.openweathermap.org/data/2.5/weather?zip=84790&units=imperial&appid=5e8bfdfbf705960fd8680dfebfbf0726';
+let apiURL =
+  "https://api.openweathermap.org/data/2.5/weather?zip=84790&units=imperial&appid=5e8bfdfbf705960fd8680dfebfbf0726";
 
 fetch(apiURL)
-    .then((response) => response.json())
-    .then((jsObject) => {
-        //console.log(jsObject);
+  .then((response) => response.json())
+  .then((jsObject) => {
+    //console.log(jsObject);
 
-        const currentTemp = document.querySelector('#current-temp');
-        const weatherIcon = document.querySelector('#weathericon');
+    const currentTemp = document.querySelector("#current-temp");
+    const weatherIcon = document.querySelector("#weathericon");
 
-        const caption = document.querySelector('figcaption');
+    const caption = document.querySelector("figcaption");
 
-        currentTemp.textContent = jsObject.main.temp.toFixed(0);
+    currentTemp.textContent = jsObject.main.temp.toFixed(0);
 
-        let imgsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
+    let imgsrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
 
-        let imgalt = jsObject.weather[0].description;
+    let imgalt = jsObject.weather[0].description;
 
-        currentTemp.textContent = jsObject.main.temp.toFixed(0);
-        weatherIcon.setAttribute('src', imgsrc);
-        weatherIcon.setAttribute('alt', imgalt);
-        caption.innerHTML = `Currently: ${imgalt}`;
-    });
-
+    currentTemp.textContent = jsObject.main.temp.toFixed(0);
+    weatherIcon.setAttribute("src", imgsrc);
+    weatherIcon.setAttribute("alt", imgalt);
+    caption.innerHTML = `Currently: ${imgalt}`;
+  });
