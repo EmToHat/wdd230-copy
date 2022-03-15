f8f2517b90f585843f9f6fd3af45f79d17fee0d2
emt0rres <emilee@hatchweb.com>
2022-03-14 22:06:40 -0600

    updates

--- chamber/js/index.js
+++ chamber/js/index.js
@@ -1,91 +1,87 @@
-// Lazy Load Images
+// _____Lazy Load Images____
 function lazyLoad() {
-    // variable to get all imgs w/data-src  attribute
-const imagesToLoad = document.querySelectorAll("img[data-src]");
+  // variable to get all imgs w/data-src  attribute
+  const imagesToLoad = document.querySelectorAll("img[data-src]");
 
-
-// arrow function which processes the data-src attributes
-// function expression
-const loadImages = (image) => {
+  // arrow function which processes the data-src attributes
+  // function expression
+  const loadImages = (image) => {
     image.setAttribute("src", image.getAttribute("data-src"));
     image.onload = () => {
-        image.removeAttribute("data-src");
+      image.removeAttribute("data-src");
+    };
   };
-};
 
-
-imagesToLoad.forEach((img) => {
+  imagesToLoad.forEach((img) => {
     loadImages(img);
-});
+  });
 
-// if this works on the browser
-if("IntersectionObserver" in window) {
-const observer = new IntersectionObserver((items, observer) => {
-    items.forEach((item) => {
-    if(item.isIntersecting) {
-        loadImages(item.target);
-        observer.unobserve(item.target);
-    }
+  // if this works on the browser
+  if ("IntersectionObserver" in window) {
+    const observer = new IntersectionObserver((items, observer) => {
+      items.forEach((item) => {
+        if (item.isIntersecting) {
+          loadImages(item.target);
+          observer.unobserve(item.target);
+        }
+      });
     });
-});
-imagesToLoad.forEach((img) => {
-    observer.observe(img);
-});
-// Loads images
-} else {
     imagesToLoad.forEach((img) => {
-    loadImages(img);
-});
+      observer.observe(img);
+    });
+    // Loads images
+  } else {
+    imagesToLoad.forEach((img) => {
+      loadImages(img);
+    });
+  }
 }
+/*__________________________________*/
+
+// _____Updates____
+function updates() {
+  let lastUpdate = document.lastModified;
+  document.getElementById("modified").innerHTML = lastUpdate;
+
+  const date = new Date();
+  const year = date.getFullYear();
+  document.getElementById("year").innerHTML = year;
 }
+/*__________________________________*/
 
-
-
-// Updates 
-function updates()
-{
-    let lastUpdate = document.lastModified;
-    document.getElementById("modified").innerHTML = lastUpdate;
-
-    const date = new Date();
-    const year = date.getFullYear();
-    document.getElementById("year").innerHTML = year;
+// _____Hamburger Menu____
+function toggleMenu() {
+  document
+    .getElementsByClassName("navigation")[0]
+    .classList.toggle("responsive");
 }
+/*__________________________________*/
 
 
 
-// Hamburger Menu 
-function toggleMenu()
-{
-	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
-}
-
-
-
+// _____Greeting Banner____
 //const now = new Date()
 //date = new Intl.DateTimeFormat().format(now)
 
-function dayOfweek(){
-    let message;
+function dayOfweek() {
+  let message;
 
-    let currentDate = new Date();
-    weekdayValue = currentDate.getDay();
+  let currentDate = new Date();
+  weekdayValue = currentDate.getDay();
 
-    if (weekdayValue == 1) 
-    {
-        message = "Join us for the chamber meet & greet Wednesday at 7:00 p.m.";
-        document.getElementById("greeting").innerHTML = message;
-    }
-    /*else if (weekdayValue == 3)
+  if (weekdayValue == 1) {
+    message = "Join us for the chamber meet & greet Wednesday at 7:00 p.m.";
+    document.getElementById("greeting").innerHTML = message;
+  } else {
+  /*else if (weekdayValue == 3)
     {
         message = "Come join us for the chamber meet and greet Tonight at 7:00 p.m.";
         document.getElementById("greeting").innerHTML = message;
     }*/
-    else 
-    {
-        document.getElementById("greeting").style.display = "none";
-    }
+    document.getElementById("greeting").style.display = "none";
+  }
 }
+/*__________________________________*/
 
 //Output
 lazyLoad();
