97f6e73d6ed0a2373424664924c99dce00d28da5
emt0rres <emilee@hatchweb.com>
2022-04-06 15:49:07 -0600

    updates

--- templeInnSuites/js/index.js
+++ templeInnSuites/js/index.js
@@ -7,3 +7,14 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+function updates() {
+  let lastUpdate = document.lastModified;
+  document.getElementById("modified").innerHTML = lastUpdate;
+
+  const date = new Date();
+  const year = date.getFullYear();
+  document.getElementById("year").innerHTML = year;
+}
+
+updates()
